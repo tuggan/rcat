97894233dc9110556301cc78b45e2d0cb2250560
Dennis Vesterlund <dennisvesterlund@gmail.com>
2017-06-06 14:33:11 +0200

    Reads all arguments as files and prints the first 1024 bytes to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+use std::io::{self, Write};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", &args[1..]);
+
+    for file in &args[1..] {
+        readfile(file);
+    }
+}
+
+fn readfile(file: &String) {
+    let fd = File::open(file);
+    if fd.is_ok() {
+        println!("{}: File exists!", file);
+
+        let mut fdu = fd.unwrap();
+
+        let metadata = fdu.metadata().unwrap();
+
+        if metadata.is_file() {
+            println!("Reading file {}", file);
+            let mut buffer = [0; 1024];
+
+            fdu.read(&mut buffer);
+
+            io::stdout().write(&buffer);
+            //let mut buf_reader = BufReader::new(fdu);
+        }
+
+    } else {
+        println!("{}: No such file!", file);
+    }
 }
